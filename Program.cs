603c84598d17f5887c2933a3b5bd43244d8a52e0
sange17 <sange17@naver.com>
2023-09-08 19:54:35 +0900

    Add mathematics_18411.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,31 @@ class Program
 {
   static void Main(string[] args)
   {
-    Mathematics_22015 func = new Mathematics_22015();
+    Mathematics_18411 func = new Mathematics_18411();
 
     func.solve();
   }
 }
+
+// using System;
+
+// namespace baekjoon
+// {
+//   class Mathematics_22015
+//   {
+//     public void solve()
+//     {
+//       string line = Console.ReadLine();
+
+//       int A = Int32.Parse(line.Split(" ")[0]);
+//       int B = Int32.Parse(line.Split(" ")[1]);
+//       int C = Int32.Parse(line.Split(" ")[2]);
+
+//       int max = Math.Max(Math.Max(A, B), C);
+
+//       int sum = (max - A) + (max - B) + (max - C);
+
+//       System.Console.WriteLine(sum);
+//     }
+//   }
+// }
